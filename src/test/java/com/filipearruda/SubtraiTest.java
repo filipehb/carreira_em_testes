20f49a3d9dec430cc44002699cde85e13f043614
Filipe Arruda <filipehenriquester@gmail.com>
2023-11-14 17:56:03 -0300

    - Adicionado testes do subtrair

--- src/test/java/com/filipearruda/SubtraiTest.java
+++ src/test/java/com/filipearruda/SubtraiTest.java
@@ -7,6 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubtraiTest {
 
     @Test
-    void subtrair() {
+    public void testSubtrairNumerosPositivos() {
+        float resultado = Subtrai.subtrair(5.0f, 3.0f);
+        assertEquals(2.0f, resultado, 0.001f); // 0.001f é a margem de erro permitida
+    }
+
+    @Test
+    public void testSubtrairNumerosNegativos() {
+        float resultado = Subtrai.subtrair(-5.0f, -3.0f);
+        assertEquals(-2.0f, resultado, 0.001f);
+    }
+
+    @Test
+    public void testSubtrairZero() {
+        float resultado = Subtrai.subtrair(0.0f, 0.0f);
+        assertEquals(0.0f, resultado, 0.001f);
     }
 }
